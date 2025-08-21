fbe47c2fc6a116c838af389c853b47ba811f9cf5
Prabhjot Singh <prabhjotsingh394@gmail.com>
2025-08-21 11:01:22 +0530

    Perform input data validations inside accounts microservice

--- accounts/src/main/java/com/gcorporation/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/gcorporation/accounts/controller/AccountsController.java
@@ -10,17 +10,19 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
+@Validated
 public class AccountsController {
 
     private IAccountsService iAccountsService;
 
     @PostMapping("/create")
-    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+    public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
         iAccountsService.createAccount(customerDto);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
